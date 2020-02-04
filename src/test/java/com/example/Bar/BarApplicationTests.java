b399ee0661d9fbb7ffcd2658c8bfe630e54ef6b9
rizhiy04 <denis20061999@gmail.com>
2020-02-04 21:46:55 +0300

    Initial commit1

---
+++ src/test/java/com/example/Bar/BarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Bar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
